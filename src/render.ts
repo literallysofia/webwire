2180fe91bb92fa466d484a976dde327380f58c5b
Sofia Silva <up201505628@fe.up.pt>
2020-02-21 19:09:35 +0000

    render :white_check_mark:

--- src/render.ts
+++ src/render.ts
@@ -1,6 +1,18 @@
-const rough = require( 'roughjs/bundled/rough.cjs.js');
+const rough = require("roughjs/bundled/rough.cjs.js");
+var data = require("../data.json");
+
+console.log(data.elements);
 
 let svgCanvas = document.getElementById("canvas") as HTMLElement;
 const rc = rough.svg(svgCanvas);
-let node = rc.rectangle(10, 10, 200, 200); // x, y, width, height
-svgCanvas.appendChild(node);
+
+var maxHeight = 0;
+
+for (let e of data.elements) {
+  let node = rc.rectangle(e.x, e.y, e.width, e.height, { roughness: 1.5 });
+  svgCanvas.appendChild(node);
+
+  if (maxHeight < e.height + e.y) maxHeight = e.height + e.y;
+}
+
+svgCanvas.setAttribute("height", (maxHeight + 30).toString());
